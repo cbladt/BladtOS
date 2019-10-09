24bc5944c3701e20b173c1c142ab7e60015b1e4e
Casper Bladt <casperbladt@gmail.com>
2019-10-09 15:32:38 +0200

    test

--- Kernel/Utility/Types.cpp
+++ Kernel/Utility/Types.cpp
@@ -1,3 +1,4 @@
 #include "Types.hpp"
 // TODO Remove this file as soon as the UtilityLib contains another translation unit so that CMake recognises it as a lib.
 // TODO THIS IS A TEST
+// TODO This is a new test
