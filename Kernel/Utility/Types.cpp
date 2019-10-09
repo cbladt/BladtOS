04d1a30a80262467a64ccd20477df17e80e2af03
Casper Bladt <casperbladt@gmail.com>
2019-10-09 16:26:11 +0200

    removed debug info

--- Kernel/Utility/Types.cpp
+++ Kernel/Utility/Types.cpp
@@ -1,24 +1,2 @@
 #include "Types.hpp"
 // TODO Remove this file as soon as the UtilityLib contains another translation unit so that CMake recognises it as a lib.
-// TODO THIS IS A TEST
-
-// TODO This is a new test
-// TODO This is a new test
-// TODO This is a new test
-// TODO This is a new test// TODO This is a new test
-// TODO This is a new test123
-// TODO This is a new test1234
-// TODO This is a new test
-// TODO This is a new test124354564
-// TODO This is a new test
-// TODO This is a new test
-// TODO This is a new test234234234
-// TOTO abekat
-// TODO FISK
-// TODO FISK
-// TODO FISK
-// TODO FISK1234
-// TODO FISK1234
-// TODO FISK1234
-// TODO FISK1234asdasdasodiasid
-
